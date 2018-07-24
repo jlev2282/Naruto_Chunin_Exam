36bb91f5059d592ece38242f9b3932ab31c8f22d
John Leverett <jleverett@gls.com>
2018-07-24 10:06:59 -0400

    added basic character images and addition to screen

--- assets/naruto.js
+++ assets/naruto.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     var gameVariables = {
         instructions : "There are four rounds to the Chunin exam. In each round you will have to defeat your opponent by" +
-                       " using your attack skills to reduce their health points to zero, but be careful. Each time you attack" +
+                       " using your attack skills to reduce their health points to zero, but be careful. Each time you attack," +
                        " your opponent will attack in return, reducing your health points by the level of their attack. Choose" +
                        " your opponents wisely because your health point will not regenerate. With each attack, your confidence and" +
                        " ability will grow making each subsequent attack stronger than the previous one.",
@@ -13,9 +13,18 @@ $(document).ready(function(){
             info.fadeIn(3000);
             setTimeout(function(){info.html("<p>"+gameVariables.instructions+"</p>")}, 2000);
             //show all the characters available to choose from
-
+            gameVariables.show_characters();
+        },
+        show_characters: function(){
+            $("#characters").fadeIn(4000);
+            character_array = gameVariables.characters;
+            for(i=0;i<character_array.length;i++){
+                html = "<div class='col-md'><img src='"+character_array[i]+"' class='character-image'></div>";
+                $("#characters").append(html);
+            }
         },
         hint: "Maybe start with a weaker opponent and hone your attack ability on them before moving on to stronger foes?",
+        characters: ["assets/images/naruto_ready.jpeg","assets/images/sakura_ready.jpeg","assets/images/sasuke_ready.jpeg","assets/images/shikamaru_ready.png","assets/images/neji_ready.jpg"],
 
 
     };
