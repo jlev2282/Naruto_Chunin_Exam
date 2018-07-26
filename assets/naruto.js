d5a8e798f8db0c36ee31f8d9e18d37a7ed795e4a
John <jlev2282@gmail.com>
2018-07-26 00:40:18 -0400

    added stats buttons and min height with margins

--- assets/naruto.js
+++ assets/naruto.js
@@ -6,11 +6,12 @@ $(document).ready(function(){
         opponent: null,
         round: 1,
         ready2fight: false,
-        instructions : "There are four rounds to the Chunin exam. In each round you will have to defeat your opponent by" +
+        instructions : "This year's Chunin exams are hosted by the Village Hidden in the Leaves. There are four rounds to the exam. In each round you will have to defeat your opponent by" +
                        " using your attack skills to reduce their chakra(life force) to zero, but be careful. Each time you attack," +
                        " your opponent will attack in return, reducing your chakra by the level of their attack. Choose" +
                        " your opponents wisely because your chakra will not regenerate. With each attack, your confidence and" +
-                       " ability will grow making each subsequent attack stronger than the previous one.",
+                       " ability will grow making each subsequent attack stronger than the previous one. Mouse over each character to learn their strengths"+
+                       " and when ready, click your choice to begin.",
         start: function(){
             //clear the banner div and replace with game instructions
             info = $("#user-info");
@@ -24,7 +25,7 @@ $(document).ready(function(){
                 info_body.empty();
                 }, 1500);
             //start to to fade back in banner to show new instructions
-            info.fadeIn(3000);
+            info.fadeIn(2500);
             //banner should be faded out by now and fire code to fade back in banner a
             setTimeout(function(){
                 info_header.text("INSTRUCTIONS");
@@ -135,6 +136,46 @@ $(document).ready(function(){
                 $("#user-info-body").html(html);
             }, 2000);
             gameVariables.ready2fight = true;
+        },
+        startRound: function(){
+            //change background to arena field 
+            // $('body').css('background-image', 'url("images/arena_field_grass.jpg")');
+            // body.style.backgroundImage = 'url(images/arena_field_grass.jpg';
+
+            $("#user-info-header").fadeOut(1500);
+            $("#user-info-body").fadeOut(1500);
+            //empty the banner completely after 1.5 seconds
+            setTimeout(function(){
+                $("#user-info-header").empty();
+                $("#user-info-body").empty();
+            }, 1500);
+
+            $("#user-info-body").fadeIn(2500);
+            $("#user-info-header").fadeIn(2500);
+
+            setTimeout(function(){
+                $("#user-info-header").text("ROUND "+gameVariables.round);
+                $("#user-info-body").html("<h3>"+gameVariables.character+" VS "+gameVariables.opponent+"</h3");
+            }, 2000);
+
+            gameVariables.judgeRound();
+
+        },
+        judgeRound: function(){
+            setTimeout(function(){
+                //get the fighting statistics of each player
+                if(gameVariables.round == 1){
+                    myFighter = gameVariables.getCharacter(gameVariables.character);
+                    myOpponent = gameVariables.getCharacter(gameVariables.opponent);
+                    fightPanel = "<div class='row' style='margin-top: 25px;'><div class='col'><button class='btn btn-warning btn-block' id='attack'>ATTACK</button></div></div>";
+                    fightPanel2 = "<div class='row' id='stats'><div class='col' id='characterStats></div><div class='col' id='opponentStats></div></div>";
+                    $('#actions').append(fightPanel2, fightPanel);
+                    
+                }
+                //assign those to variables for this round only
+                //create 2 small divs with a button to attack, and the 3 statistics for each fighter
+                //append those divs to #character and #opponent
+            }, 1000);
         }
     };
 
@@ -150,10 +191,10 @@ $(document).ready(function(){
         if(gameVariables.gameStarted == false) {
 
             html = "<div class='card text-center' style='width: 18rem; max-height: 100%;'><img class='card-img-top' style='height: 22vh;' src='"+character.win_pic+"' alt='"+currentCharacter+"'>"+
-                    "<div class='card-header' style='max-height: 7vh;'><h5 class='card-title'>"+character.name+"</h5></div>"+
-                    "<div class='card-body><p class='card-text'>Favorite Saying: \""+character.saying+"\"</p></div><ul class='list-group list-group-flush'>"+
-                    "<li class='list-group-item'>Chakra: "+character.chakra+"</li><li class='list-group-item'>Attack: "+character.attack_power+"</li>"+
-                    "<li class='list-group-item'>Counter Attack: "+character.counter_attack_power+"</li></ul></div>";
+                    "<div class='card-header card-styling'><h5 class='card-title'>"+character.name+"</h5></div>"+
+                    "<div class='card-body><p class='card-text card-styling'>Favorite Saying: \""+character.saying+"\"</p></div><ul class='list-group list-group-flush'>"+
+                    "<li class='list-group-item card-styling'>Chakra: "+character.chakra+"</li><li class='list-group-item'>Attack: "+character.attack_power+"</li>"+
+                    "<li class='list-group-item card-styling'>Counter Attack: "+character.counter_attack_power+"</li></ul></div>";
 
             $("#actions").append(html);
             // $("#opponent").append(traitList);
@@ -268,6 +309,8 @@ $(document).ready(function(){
                 }
                 
             }
+
+            gameVariables.startRound();
 
         }
     });
